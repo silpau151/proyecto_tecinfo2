1ae8f77e671e631a80e3ae6a9840d97059319256
silpau151 <a100241977@my.uvm.edu.mx>
2025-04-30 14:06:50 -0600

    mi_script.js

--- mi_script.js
+++ mi_script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const miBoton = document.getElementById('miBoton');
 
     miBoton.addEventListener('click', function() {
-        alert('¡Hola! Este es un botón interactivo.');
+        alert('¡Hola desde JavaScript!');
+        // Aquí puedes agregar más funcionalidades al botón
     });
 });
